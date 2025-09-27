79f84cc03d7d608e6a119bf2724c67a1b59ba355
xqyr <2513777371@qq.com>
2025-09-27 21:50:28 +0800

    add:基本框架

--- plot/plotcustom.h
+++ plot/plotcustom.h
@@ -8,15 +8,11 @@
 #include <qcustomplot/qcustomplot.h>
 #include "plotmodel.h"
 class QTimer;
-class QTimer;
 class PlotCustom:public QCustomPlot{
     Q_OBJECT
 public:
     explicit PlotCustom(QWidget *parent = nullptr);
     void resetUI( PlotModel* model);
-    int getPlotType()const{ return plotType; }
-public slots:
-    void setPlotType(int type);
     int getPlotType()const{ return plotType; }
 public slots:
     void setPlotType(int type);
@@ -32,12 +28,7 @@ private:
 private:
    QTimer* _updateTimer;
     QTimer* _resetUITimer;
-    QTimer* _resetUITimer;
     bool _updateTimerEn;
-    PlotModel* _model;
-    int plotType;
-private:
-    // Q_PROPERTY(int getPlotType READ plotType WRITE setPlotType)
     PlotModel* _model;
     int plotType;
 private:
